343355bc7d485391facfe6fed73056966bb322b5
Gopal <gsudhanshu12@gmail.com>
2017-12-30 15:10:07 -0500

    recipe added

--- src/app/recipebook/recipe-list/recipe-list.component.ts
+++ src/app/recipebook/recipe-list/recipe-list.component.ts
@@ -6,9 +6,10 @@ import { Recipe } from '../recipebook.model'
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-recipse: Recipe[]=[
+recipes: Recipe[]=[
   new Recipe('A test Recipe','This is a simple recipe',
   'http://maxpixel.freegreatpicture.com/static/photo/1x/Food-Chicken-Menu-Recipe-Bacon-Dinner-Meat-Dish-2428926.jpg')
+
 ];
   constructor() { }
 
